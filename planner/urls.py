7d3292b05a01b617dd816463c5e5028cb8bc9f31
Shubhangi <shubhangi.khasnis7@gmail.com>
2017-10-21 19:37:58 +0530

    Added login page and signup page

--- planner/urls.py
+++ planner/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.login_page, name='login_page'),
+    url(r'^sign_?up/?$',views.sign_up, name='sign_up'),
 ]
--- planner/views.py
+++ planner/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate
 from .models import Event
+from .forms import SignUpForm
 # Create your views here.
 def login_page(request):
     events=Event.objects.all()
     return render(request, 'planner/login_page.html', {'events':events})
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('login_page')
+    else:
+        form = SignUpForm()
+    return render(request, 'planner/sign_up.html', {'form': form})
+    
